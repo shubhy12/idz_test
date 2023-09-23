4f1567f78030e1ad73c3f1a96832d6e360bf1429
shubhycomxr <shubham.shetye@comxr.in>
2023-09-23 13:50:20 +0530

    - added gameover panel - Mainmenu, SequencingScene  and Looping scene added

---
+++ Assets/Scripts/UI/GameOverPanel.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverPanel : MonoBehaviour
+{
+    public TextMeshProUGUI heading;
+
+    private void OnEnable()
+    {
+
+    }
+    public void ShowGameOver(bool won)
+    {
+        gameObject.SetActive(true);
+        if (won)
+        {
+            heading.text = "Level Cleared!";
+        }
+        else
+        {
+            heading.text = "Failed!";
+        }
+    }
+
+    public void OnClickRestar()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void GoToHome()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
