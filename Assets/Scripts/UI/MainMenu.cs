298b5c668416eecaa17e7b056cc4fd51a0861a59
shubhycomxr <shubham.shetye@comxr.in>
2023-09-25 15:42:21 +0530

    - UI updated - Scenes updated

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -5,20 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void OnClickScene(int sceneId)
     {
         SceneManager.LoadScene(sceneId);
+    }
+    public void OnClose()
+    {
+        Application.Quit();
     }
 }
